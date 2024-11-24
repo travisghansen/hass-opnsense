c0387837516401c743349e434824a8b149bd0714
Snuffy2 <Snuffy2@users.noreply.github.com>
2024-11-24 12:16:04 -0500

    Add reconfigure

--- custom_components/opnsense/const.py
+++ custom_components/opnsense/const.py
@@ -39,6 +39,7 @@ CONF_DEVICE_TRACKER_CONSIDER_HOME = "device_tracker_consider_home"
 DEFAULT_DEVICE_TRACKER_CONSIDER_HOME = 0
 
 CONF_DEVICE_UNIQUE_ID = "device_unique_id"
+CONF_FIRMWARE_VERSION = "firmware_version"
 
 CONF_DEVICES = "devices"
 CONF_MANUAL_DEVICES = "manual_devices"
